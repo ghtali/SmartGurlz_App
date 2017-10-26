87b9e07cf2650456820803ef6a60523b47f85576
Ali.Gh <ght.ali@hotmail.com>
2017-10-26 16:37:34 +0200

    SharedPreferences first try
    SharedPreferences to store a boolean value indicating first time launch.

--- app/src/main/java/smartgurlz/com/smartgurlz/PrefManager.java
+++ app/src/main/java/smartgurlz/com/smartgurlz/PrefManager.java
@@ -1,8 +1,52 @@
 package smartgurlz.com.smartgurlz;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by Ali on 26/10/2017.
  */
 
 public class PrefManager {
+
+    //Used SharedPreferences to store a boolean value indicating first time launch
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    //Shared pref mode
+
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+
+    private static final String PREF_NAME = "smartgurlz-welcome";
+
+    private static final String Is_First_Time_Launch = "IsFirstTimeLaunch";
+
+    public PrefManager (Context context){
+
+        this._context = context;
+
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+
+        editor = pref.edit();
+
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime){
+
+        editor.putBoolean(Is_First_Time_Launch, isFirstTime);
+        editor.commit();
+
+    }
+
+    public boolean isFirstTimeLaunch(){
+        return pref.getBoolean(Is_First_Time_Launch, true);
+    }
+
+
+
+
 }
