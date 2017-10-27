6a78ccd49abd1998489567f69211a4a4266878d9
Ali.Gh <ght.ali@hotmail.com>
2017-10-27 13:48:28 +0200

    Settingup slides 1-4

---
+++ app/src/main/java/smartgurlz/com/smartgurlz/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package smartgurlz.com.smartgurlz;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends FragmentActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+
+    }
+}
