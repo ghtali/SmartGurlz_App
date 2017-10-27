f2f4544cb2dadfdb1b06f03fc4f16e0e9b4d41af
Ali.Gh <ght.ali@hotmail.com>
2017-10-27 13:58:14 +0200

    Checking for first time launch

--- app/src/main/java/smartgurlz/com/smartgurlz/WelcomeActivity.java
+++ app/src/main/java/smartgurlz/com/smartgurlz/WelcomeActivity.java
@@ -1,9 +1,11 @@
 package smartgurlz.com.smartgurlz;
 
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,9 +23,19 @@ public class WelcomeActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         // Checking for first time launch - before calling setContentView()
-        prefManager
+        prefManager = new PrefManager(this);
+        if (!prefManager.isFirstTimeLaunch()){
+            launchHomeScreen();
+            finish();
+        }
+
+        // Making notification bar transparent
+
+        if (Build.VERSION.SDK_INT >= 21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
 
 
         setContentView(R.layout.activity_welcome);
