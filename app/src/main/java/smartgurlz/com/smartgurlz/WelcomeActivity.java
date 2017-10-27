8b4313c75cfbac8318b22d2b6a4537e6c1d9fa17
Ali.Gh <ght.ali@hotmail.com>
2017-10-27 13:53:06 +0200

    init WelcomeActivity

--- app/src/main/java/smartgurlz/com/smartgurlz/WelcomeActivity.java
+++ app/src/main/java/smartgurlz/com/smartgurlz/WelcomeActivity.java
@@ -1,17 +1,34 @@
 package smartgurlz.com.smartgurlz;
 
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class WelcomeActivity extends FragmentActivity {
 
-    
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
+    private PrefManager prefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Checking for first time launch - before calling setContentView()
+        prefManager
+
+
         setContentView(R.layout.activity_welcome);
+
+
 
 
     }
