d2c69cf0fdbedf099a54f7f7187fb31749438e77
Ali.Gh <ght.ali@hotmail.com>
2017-11-28 16:40:54 +0100

    Correction on SelectLevel, LevelAdapter init

--- app/src/main/java/smartgurlz/com/smartgurlz/SelectLevel.java
+++ app/src/main/java/smartgurlz/com/smartgurlz/SelectLevel.java
@@ -1,7 +1,11 @@
 package smartgurlz.com.smartgurlz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class SelectLevel extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class SelectLevel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_level);
+
+        setContentView(R.layout.activity_select_level);
+
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new LevelAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(SelectLevel.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
