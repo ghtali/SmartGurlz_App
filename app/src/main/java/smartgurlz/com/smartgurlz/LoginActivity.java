3025eaa47fa334a03edea62d2d858eb39c09639f
Ana-Maria <anamaria.zokalj@gmail.com>
2017-11-29 14:05:00 +0100

    login update again
    new onclick

--- app/src/main/java/smartgurlz/com/smartgurlz/LoginActivity.java
+++ app/src/main/java/smartgurlz/com/smartgurlz/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +15,7 @@ import android.widget.Toast;
 
 /* @author Ana-Maria Fischer Zokalj
 */
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class LoginActivity extends FragmentActivity{
 
     private EditText username_input;
     private EditText password_input;
@@ -35,14 +36,57 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         signup_btn = (Button) findViewById(R.id.signup_bt);
         back_btn = (ImageButton) findViewById(R.id.back_btn);
 
-        login_btn.setOnClickListener(this);
-        signup_btn.setOnClickListener(this);
-        back_btn.setOnClickListener(this);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+                String name = sharedPref.getString("username", "");
+                String pw = sharedPref.getString("password", "");
+
+                //Checks if the password and username is correct. Redirects to the menu if correct.
+                if(username_input.getText().toString().equals(name) &&
+                        password_input.getText().toString().equals(pw)) {
+                    Toast.makeText(getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, MenuActivity.class);
+                    startActivity(i);
+
+                    //Or a toast will tell the user that it is wrong
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong username or password",Toast.LENGTH_LONG).show();
+
+
+                }
+            }
+        });
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("username", username_input.getText().toString());
+                editor.putString("password", password_input.getText().toString());
+                editor.apply();
+
+                Toast.makeText(LoginActivity.this, "Saved", Toast.LENGTH_LONG).show();
+
+                //so the password and username clears from the screen.
+                username_input.getText().clear();
+                password_input.getText().clear();
+            }
+        });
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MenuActivity.class);
+                startActivity(i);
+            }
+        });
 
 
     }
 
-    @Override
+ /*   @Override
     public void onClick(View v) {
 
      // save the users login info
@@ -61,7 +105,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             password_input.getText().clear();
         }
 
-    // Login button
+    //Login button
       if(v == login_btn) {
       SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
        String name = sharedPref.getString("username", "");
@@ -87,7 +131,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
       startActivity(i);
         }
     }
-
+*/
 
 
 }
