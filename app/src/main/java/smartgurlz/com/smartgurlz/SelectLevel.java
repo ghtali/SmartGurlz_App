526bb334a7d4248c23e7f5c910e8f7c6cd0a7dc9
Ali.Gh <ght.ali@hotmail.com>
2017-11-29 14:57:04 +0100

    Bug fix

--- app/src/main/java/smartgurlz/com/smartgurlz/SelectLevel.java
+++ app/src/main/java/smartgurlz/com/smartgurlz/SelectLevel.java
@@ -1,22 +1,21 @@
 package smartgurlz.com.smartgurlz;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
-public class SelectLevel extends AppCompatActivity {
+public class SelectLevel extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_level);
 
-        setContentView(R.layout.activity_select_level);
 
-        GridView gridview = (GridView) findViewById(R.id.gridView);
+        GridView gridview = (GridView) findViewById(R.id.gridview1);
         gridview.setAdapter(new LevelAdapter(this));
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
