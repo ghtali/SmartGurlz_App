2d5e261837b521257839c7af7350c773f296408e
Unknown <anamaria.zokalj@gmail.com>
2018-01-03 12:30:54 +0100

    Login Fragment init

---
+++ app/src/main/java/smartgurlz/com/smartgurlz/LoginFragment.java
@@ -0,0 +1,105 @@
+package smartgurlz.com.smartgurlz;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+
+/** @author Ana-Maria Fischer
+ * A simple {@link Fragment} subclass.
+ * 
+ */
+public class LoginFragment extends Fragment {
+
+    private EditText username_input;
+    private EditText password_input;
+    private Button login_btn;
+    private Button signup_btn;
+    private ImageButton back_btn;
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        username_input = (EditText) view.findViewById(R.id.username_etx);
+        password_input = (EditText) view.findViewById(R.id.password_etx);
+
+        login_btn = (Button) view.findViewById(R.id.login_bt);
+        signup_btn = (Button) view.findViewById(R.id.signup_bt);
+        back_btn = (ImageButton) view.findViewById(R.id.back_btn);
+
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+                String name = sharedPref.getString("username", "");
+                String pw = sharedPref.getString("password", "");
+
+                //Checks if the password and username is correct. Redirects to the menu if correct.
+                if(username_input.getText().toString().equals(name) &&
+                        password_input.getText().toString().equals(pw)) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(getActivity(), MenuActivity.class);
+                    startActivity(i);
+                    getActivity().finish();
+
+                    //Or a toast will tell the user that it is wrong
+                }else{
+                    Toast.makeText(getActivity().getApplicationContext(), "Wrong username or password",Toast.LENGTH_LONG).show();
+
+
+                }
+            }
+        });
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("username", username_input.getText().toString());
+                editor.putString("password", password_input.getText().toString());
+                editor.apply();
+
+                Toast.makeText(getActivity().getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
+
+                //so the password and username clears from the screen.
+                username_input.getText().clear();
+                password_input.getText().clear();
+            }
+        });
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), MenuActivity.class);
+                startActivity(i);
+               getActivity().finish();
+            }
+        });
+
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+}
