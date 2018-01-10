114943688e6354980ff499de0b139a9ca59f562d
Ali.Gh <ght.ali@hotmail.com>
2018-01-10 13:32:18 +0100

    UserSignup update

--- app/src/main/java/smartgurlz/com/smartgurlz/menufragments/UserSignUp.java
+++ app/src/main/java/smartgurlz/com/smartgurlz/menufragments/UserSignUp.java
@@ -1,20 +1,33 @@
 package smartgurlz.com.smartgurlz.menufragments;
 
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import smartgurlz.com.smartgurlz.R;
 
 /**
  * A simple {@link Fragment} subclass.
- *
  */
 public class UserSignUp extends Fragment {
+
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText getEditTextPassword;
+    private TextView textViewSignIn;
+    private FirebaseAuth firebaseAuth;
+
+
+
 
 
     public UserSignUp() {
@@ -26,7 +39,34 @@ public class UserSignUp extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+            firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null){
+
+            UserProfile userProfile = new UserProfile();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.userprofileLayout, userProfile).commit();
+
+            buttonRegister = getView().findViewById(R.id.buttonRegister);
+
+            editTextEmail =  getView().findViewById(R.id.editTextEmail);
+
+            getEditTextPassword =  getView().findViewById(R.id.editTextPassword);
+
+            textViewSignIn = getView().findViewById(R.id.textViewSignIn);
+
+            buttonRegister.setOnClickListener((View.OnClickListener) this);
+
+            textViewSignIn.setOnClickListener((View.OnClickListener) this);
+
+        }
+
         return inflater.inflate(R.layout.fragment_user_sign_up, container, false);
+
+
     }
+
 
 }
