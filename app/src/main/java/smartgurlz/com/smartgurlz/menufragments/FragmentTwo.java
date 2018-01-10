b07292c36df241b9956b52e7d2d1e182efecd61e
Mads <Finnerup1@gmail.com>
2018-01-10 12:22:30 +0100

    First iteration of firebase to login window

--- app/src/main/java/smartgurlz/com/smartgurlz/menufragments/FragmentTwo.java
+++ app/src/main/java/smartgurlz/com/smartgurlz/menufragments/FragmentTwo.java
@@ -6,13 +6,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.concurrent.Executor;
 
 import smartgurlz.com.smartgurlz.MainMenu;
 import smartgurlz.com.smartgurlz.R;
@@ -29,6 +39,10 @@ public class FragmentTwo extends Fragment {
     private EditText password_input;
     private Button login_btn;
     private Button signup_btn;
+    private Button forgotPassword;
+    private FirebaseAuth auth;
+    private FrameLayout login_fID;
+    private  Snackbar snackbar;
   //  private ImageButton back_btn;
 
     public FragmentTwo() {
@@ -43,17 +57,30 @@ public class FragmentTwo extends Fragment {
        // View view = inflater.inflate(R.layout.fragment_login, container, false);
         View view = inflater.inflate(R.layout.fragment_fragment_two, container, false);
 
+
         username_input = (EditText) view.findViewById(R.id.userName_etx);
         password_input = (EditText) view.findViewById(R.id.passWord_etx);
 
         login_btn = (Button) view.findViewById(R.id.login_btn);
         signup_btn = (Button) view.findViewById(R.id.signup_btn);
+        forgotPassword = (Button) view.findViewById(R.id.forgotPassword);
+
+        login_fID = (FrameLayout) view.findViewById(R.id.login_fID);
+
       //  back_btn = (ImageButton) view.findViewById(R.id.backb_btn);
 
+        // Init Firebase Auth
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            //Start aktivity
+        }
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 SharedPreferences sharedPref = getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
                 String name = sharedPref.getString("username", "");
                 String pw = sharedPref.getString("password", "");
@@ -101,9 +128,31 @@ public class FragmentTwo extends Fragment {
         });
 */
 
+       forgotPassword.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+           }
+
+       });
+
         // Inflate the layout for this fragment
 
        return inflater.inflate(R.layout.fragment_fragment_two, container, false);
+    }
+    private  void loginUser(String email, final String password) {
+        auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    if (password.length() < 6) {
+                        //Toast.makeText(, "Password is wrong", Toast.LENGTH_LONG).show();
+                        Snackbar.make(login_fID, "had a d", Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
 }
