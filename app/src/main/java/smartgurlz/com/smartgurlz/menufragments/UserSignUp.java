c4d25315ab3084c358769f78d1eefb5c66975553
Ali.Gh <ght.ali@hotmail.com>
2018-01-10 14:47:37 +0100

    UserSignup update 3

--- app/src/main/java/smartgurlz/com/smartgurlz/menufragments/UserSignUp.java
+++ app/src/main/java/smartgurlz/com/smartgurlz/menufragments/UserSignUp.java
@@ -1,16 +1,23 @@
 package smartgurlz.com.smartgurlz.menufragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import smartgurlz.com.smartgurlz.R;
@@ -41,9 +48,9 @@ public class UserSignUp extends Fragment {
         // Inflate the layout for this fragment
 
 
-            firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        if (firebaseAuth.getCurrentUser() != null){
+        if (firebaseAuth.getCurrentUser() != null) {
 
             UserProfile userProfile = new UserProfile();
             FragmentManager fragmentManager = getFragmentManager();
@@ -51,9 +58,9 @@ public class UserSignUp extends Fragment {
 
             buttonRegister = getView().findViewById(R.id.buttonRegister);
 
-            editTextEmail =  getView().findViewById(R.id.editTextEmail);
+            editTextEmail = getView().findViewById(R.id.editTextEmail);
 
-            getEditTextPassword =  getView().findViewById(R.id.editTextPassword);
+            getEditTextPassword = getView().findViewById(R.id.editTextPassword);
 
             textViewSignIn = getView().findViewById(R.id.textViewSignIn);
 
@@ -65,8 +72,44 @@ public class UserSignUp extends Fragment {
 
         return inflater.inflate(R.layout.fragment_user_sign_up, container, false);
 
+    private void registerUser() {
+
+        String email = editTextEmail.getText().toString().trim();
+        String password = getEditTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            //email is empty
+            //Toast.makeText("Please Enter Your Email!");
+            //Stop the function from executing further
+            return;
+
+        }
+        if (TextUtils.isEmpty(password)) {
+            //password is empty
+            //Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+            //Stop the function from executing further
+            return;
+        }
+
+        //if validations are ok
+        //then will first show a progressDialog
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            //user is successfully registered and logged in
+                            //we will start the profile activity here
+                            //show the user a message with Toast
+                            Toast.makeText(getActivity(this),"Registered successfuly!",Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }
+                });
 
     }
-
-
 }
