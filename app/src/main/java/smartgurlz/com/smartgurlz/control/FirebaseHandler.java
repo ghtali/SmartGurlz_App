4eb6dea259bff532505e9a9aa7c084ac9000ce8e
Ali.Gh <ght.ali@hotmail.com>
2018-01-11 14:02:35 +0100

    FirebaseHandler init. (controller)

---
+++ app/src/main/java/smartgurlz/com/smartgurlz/control/FirebaseHandler.java
@@ -0,0 +1,74 @@
+package smartgurlz.com.smartgurlz.control;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import smartgurlz.com.smartgurlz.R;
+
+public class FirebaseHandler extends AppCompatActivity {
+
+    private static final String TAG = FirebaseHandler.class.getSimpleName();
+    private TextView goldPlayerPoints;
+    private TextView silverPlayerPoints;
+    private TextView bronzePlayerPoints;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+
+    private String userId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Displaying toolbar icon
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        goldPlayerPoints = findViewById(R.id.goldPlayerPoints);
+        silverPlayerPoints = findViewById(R.id.silverPlayerPoints);
+        bronzePlayerPoints = findViewById(R.id.bronzePlayerPoints);
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+
+        // get reference to 'users' node
+        mFirebaseDatabase = mFirebaseInstance.getReference("users");
+
+        // store app title to 'app_title' node
+        mFirebaseInstance.getReference("app_title").setValue("Realtime Database");
+
+        // app_title change listener
+        mFirebaseInstance.getReference("app_title").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+               // Log.d(TAG, "Value is: " + HighScore);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.e(TAG, "Failed to read app title value.", error.toException());
+            }
+        });
+    }
+
+    private void updateUser(String name, String email) {
+        // updating the user via child nodes
+        if (!TextUtils.isEmpty(name))
+            mFirebaseDatabase.child(userId).child("name").setValue(name);
+
+        if (!TextUtils.isEmpty(email))
+            mFirebaseDatabase.child(userId).child("email").setValue(email);
+    }
+}
