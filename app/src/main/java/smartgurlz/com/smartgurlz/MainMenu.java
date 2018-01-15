07539353bd69c52c2f60c783e838f19b3b818125
Flemming Larsen <s083909@student.dtu.dk>
2018-01-15 11:51:12 +0100

    Navigation and level select welcome
    Removed text from menu, made icons bigger and updated the level welcome design

--- app/src/main/java/smartgurlz/com/smartgurlz/MainMenu.java
+++ app/src/main/java/smartgurlz/com/smartgurlz/MainMenu.java
@@ -72,8 +72,6 @@ public class MainMenu extends AppCompatActivity implements LevelFragment.OnListF
                     fragmentTransaction2.replace(R.id.container, fragment2 , "FragmentName");  // container is the id of Layout in xml file()
                     fragmentTransaction2.commit();
 
-                    // Set view to Welcomescreen to gone, making it non-selectable
-                    //findViewById(R.id.navigation_welcome).setVisibility(View.GONE);
                     return true;
                 case R.id.navigation_notifications:
                     setTitle("Fragment Three");
@@ -81,8 +79,7 @@ public class MainMenu extends AppCompatActivity implements LevelFragment.OnListF
                     FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction3.replace(R.id.container, fragment3, "FragmentName"); // container is the id of Layout in xml file()
                     fragmentTransaction3.commit();
-                    // Set view to Welcomescreen to gone, making it non-selectable
-                    //findViewById(R.id.navigation_welcome).setVisibility(View.GONE);
+
                     return true;
                 case R.id.navigation_welcome:
                     setTitle("Welcome Fragment");
@@ -90,8 +87,7 @@ public class MainMenu extends AppCompatActivity implements LevelFragment.OnListF
                     FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction4.replace(R.id.container, fragment4, "FragmentName"); // container is the id of Layout in xml file()
                     fragmentTransaction4.commit();
-                    // Set view to Welcomescreen to gone, making it non-selectable
-                    //findViewById(R.id.navigation_welcome).setVisibility(View.GONE);
+
                     return true;
             }
             return false;
@@ -106,17 +102,17 @@ public class MainMenu extends AppCompatActivity implements LevelFragment.OnListF
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        //Changes the size of the menu items
+        //Changes the bottom navigtion menu items
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
         BottomNavigationMenuView menuView = (BottomNavigationMenuView) bottomNavigationView.getChildAt(0);
         for (int i = 0; i < menuView.getChildCount(); i++) {
             final View iconView = menuView.getChildAt(i).findViewById(android.support.design.R.id.icon);
             final ViewGroup.LayoutParams layoutParams = iconView.getLayoutParams();
             final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-            // New hight is set on icons
-            layoutParams.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 32, displayMetrics);
-            // New hight is set on icons
-            layoutParams.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 32, displayMetrics);
+            // New size 40 is set on icons
+            layoutParams.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, displayMetrics);
+            // New size 40 is set on icons
+            layoutParams.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, displayMetrics);
             iconView.setLayoutParams(layoutParams);
         }
 
@@ -133,7 +129,8 @@ public class MainMenu extends AppCompatActivity implements LevelFragment.OnListF
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.container, fragment, "FragmentName"); // fram is the id of FrameLayout in xml file()
         fragmentTransaction.commit();
-        // Set view to Welcomescreen to gone, making it non-selectable
+        // Set view to Welcomescreen to View.GONE,
+        // making it invisible and non-selectable
         findViewById(R.id.navigation_welcome).setVisibility(View.GONE);
     }
 
